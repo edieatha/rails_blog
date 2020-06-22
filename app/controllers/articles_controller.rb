631b53b81795852d8ca4d4374630f994e7e3dc99
Edie Atha <edieatha@gmail.com>
2020-06-22 22:16:16 +0300

    Add comment feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
@@ -25,7 +28,6 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
     flash.notice = "Article '#{@article.title}' Deleted!"
-
 
     redirect_to articles_url
   end
