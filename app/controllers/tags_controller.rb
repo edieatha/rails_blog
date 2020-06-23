e1db90b767d20c3eefd67cdd58fe66455aa653af
Edie Atha <edieatha@gmail.com>
2020-06-23 14:47:36 +0300

    Addede a few gems

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def index
     @tag = Tag.all
-   end
+  end
 
   def show
     @tag = Tag.find(params[:id])
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
 
   def tag_list
     self.tags.collect do |tag|
@@ -15,3 +18,4 @@ class Article < ApplicationRecord
     self.tags = new_or_found_tags
   end
 end
+
